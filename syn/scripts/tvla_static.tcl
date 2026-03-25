bb20c0f956290649dbd788104218290d3669ed18
jiongsin <jiongsinsoo021026@gmail.com>
2026-03-25 15:47:45 +0800

    enable tvla static TCL

--- syn/scripts/tvla_static.tcl
+++ syn/scripts/tvla_static.tcl
@@ -9,7 +9,7 @@ set PERIOD $env(PERIOD)
 set NTL ${DESIGN}_MODE${MODE}
 set NTL_TIME ${NTL}_10p0ns
 
-file mkdir ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla
+file mkdir ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla_static
 
 read_verilog ${NTL_TIME}_ntl.v
 link_design ${NTL}
@@ -25,31 +25,31 @@ read_sdf ./results/${NTL_TIME}/${NTL_TIME}.sdf
 # Annotate Activity from VCS Simulation Using time-based mode for FSDB
 set_app_var power_analysis_mode time_based
 
-#read_fsdb ./results/${NTL_TIME}/sim/${DESIGN}.fsdb \
+#read_fsdb ./results/${NTL_TIME}/sim_static/${DESIGN}.fsdb \
     -strip_path aes_operation_tb/dut \
     -time {105 450075}
-read_vcd ./results/${NTL_TIME}/sim/${DESIGN}.vcd \
+read_vcd ./results/${NTL_TIME}/sim_static/${DESIGN}.vcd \
     -strip_path aes_operation_tb/dut \
     -time {105 450075}
 
 # Check and Report Activity
-report_switching_activity > ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla/switching_activity.rpt
-report_annotated_delay > ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla/annotated_delay.rpt
+report_switching_activity > ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla_static/switching_activity.rpt
+report_annotated_delay > ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla_static/annotated_delay.rpt
 
 check_power
 
 # Configure Power Analysis Options
 # set_power_analysis_options -waveform_format fsdb \
                            -waveform_interval 0.001 \
-                           -waveform_output ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla/tvla_traces
+                           -waveform_output ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla_static/tvla_traces
 # update_power
 
 set_power_analysis_options -waveform_format out \
                            -waveform_interval 0.001 \
-                           -waveform_output ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla/tvla_traces
+                           -waveform_output ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla_static/tvla_traces
 update_power
 
-report_power > ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla/power.rpt
+report_power > ./results/${DESIGN}_MODE${MODE}_10p0ns/tvla_static/power.rpt
 
 print_message_info
 
