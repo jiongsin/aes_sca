b3a09480aa4986a187d6187701ff45c6ca6a4919
jiongsin <jiongsinsoo021026@gmail.com>
2026-04-12 22:00:33 +0800

    define new path of saed32nm PDK

--- syn/scripts/dc_lib_setup_32nm.tcl
+++ syn/scripts/dc_lib_setup_32nm.tcl
@@ -1,32 +1,36 @@
-##Load Design, Libraries
+## Set Library Base Path
+set LIB_PATH "/home/host/libs/saed32nm/lib"
+
+## Load Design, Libraries
 set_app_var target_library "saed32lvt_ss0p95v125c.db saed32hvt_ss0p95v125c.db saed32rvt_ss0p95v125c.db"
 set_app_var link_library "* $target_library saed32sram_ss0p95v125c.db"
 set_app_var search_path "$search_path . ./scripts ../rtl \
- ../libs/saed32nm/lib/stdcell_hvt/db_nldm \
- ../libs/saed32nm/lib/stdcell_lvt/db_nldm \
- ../libs/saed32nm/lib/stdcell_rvt/db_nldm \
- ../libs/saed32nm/lib/sram/db_nldm"
+ ${LIB_PATH}/stdcell_hvt/db_nldm \
+ ${LIB_PATH}/stdcell_lvt/db_nldm \
+ ${LIB_PATH}/stdcell_rvt/db_nldm \
+ ${LIB_PATH}/sram/db_nldm"
 
-set MW_REFERENCE_LIB_DIRS "../libs/saed32nm/lib/stdcell_hvt/milkyway/saed32nm_hvt_1p9m \
-                           ../libs/saed32nm/lib/stdcell_rvt/milkyway/saed32nm_rvt_1p9m \
-			   ../libs/saed32nm/lib/stdcell_lvt/milkyway/saed32nm_lvt_1p9m \
-			   ../libs/saed32nm/lib/sram/milkyway/SRAM32NM"
-set TECH_FILE "../libs/saed32nm/lib/tech/milkyway/saed32nm_1p9m_mw.tf" 
-set MAP_FILE  "../libs/saed32nm/lib/tech/star_rc/saed32nm_tf_itf_tluplus.map"  
-set TLUPLUS_MAX_FILE "../libs/saed32nm/lib/tech/star_rc/saed32nm_1p9m_Cmax.tluplus"  
-set TLUPLUS_MIN_FILE "../libs/saed32nm/lib/tech/star_rc/saed32nm_1p9m_Cmin.tluplus" 
+set MW_REFERENCE_LIB_DIRS "${LIB_PATH}/stdcell_hvt/milkyway/saed32nm_hvt_1p9m \
+                           ${LIB_PATH}/stdcell_rvt/milkyway/saed32nm_rvt_1p9m \
+                           ${LIB_PATH}/stdcell_lvt/milkyway/saed32nm_lvt_1p9m \
+                           ${LIB_PATH}/sram/milkyway/SRAM32NM"
+
+set TECH_FILE "${LIB_PATH}/tech/milkyway/saed32nm_1p9m_mw.tf" 
+set MAP_FILE  "${LIB_PATH}/tech/star_rc/saed32nm_tf_itf_tluplus.map"  
+set TLUPLUS_MAX_FILE "${LIB_PATH}/tech/star_rc/saed32nm_1p9m_Cmax.tluplus"  
+set TLUPLUS_MIN_FILE "${LIB_PATH}/tech/star_rc/saed32nm_1p9m_Cmin.tluplus" 
 
 set mw_reference_library ${MW_REFERENCE_LIB_DIRS}
 set mw_design_library MYLIB
 
 if {![file isdirectory $mw_design_library ]} {
-   create_mw_lib   -technology $TECH_FILE \
+   create_mw_lib -technology $TECH_FILE \
      -mw_reference_library $mw_reference_library \
       $mw_design_library
     } else {
       set_mw_lib_reference $mw_design_library -mw_reference_library $mw_reference_library
     }
-open_mw_lib     $mw_design_library
+open_mw_lib $mw_design_library
 
 check_library > ./reports/dc_check_lib.rpt 
 
