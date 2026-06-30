8c486ebb4a88bc2882d7662988eb92254a4d24b3
jiongsin <jiongsinsoo021026@gmail.com>
2026-06-30 17:11:27 +0800

    Revised to clean version
    # Conflicts:
    #	Makefile

--- rtl/filelist.f
+++ rtl/filelist.f
@@ -1,13 +1,10 @@
 // RTL
 $WORKAREA/rtl/aes_sbox_base.v 
-$WORKAREA/rtl/aes_sbox_opt.v
 $WORKAREA/rtl/aes_sbox_sca.v
 
 $WORKAREA/rtl/aes_operation_base.v 
-$WORKAREA/rtl/aes_operation_opt.v
 $WORKAREA/rtl/aes_operation_sca.v
 
-$WORKAREA/rtl/aes_ctr_base.v
 $WORKAREA/rtl/aes_ctr_sca.v
 
 $WORKAREA/rtl/aes_ahb_lite_dma_sca.v
